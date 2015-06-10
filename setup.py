9592bd6392901784ad1e832f5a42e93805873d7f
C.D. Clark III <clifton.clark@gmail.com>
2015-06-09 20:32:10 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+DESCRIPTION = "Config file reader and validator powered by Mako"
+LONG_DESCRIPTION = open('README.md').read()
+
+setup(name='config-makover',
+      version=0.0.0
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      author='C.D. Clark III',
+      author_email='clifton.clark@gmail.com',
+      url='https://github.com/CD3/config-makover'
+      license="MIT License",
+      platforms=["any"],
+      packages=find_packages(),
+      test_suite="",
+     )
